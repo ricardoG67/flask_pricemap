485f3a1bafe20a648ed18b96b22fe6c08afb81d9
la bella <neisser.ale@neurometrics.la>
2022-08-31 14:34:54 -0500

    trabajo 31 agosto, agregando estadisticos y filtro fecha

--- borrar.py
+++ borrar.py
@@ -1,6 +1,50 @@
-import numpy as np
+'''import numpy as np
 
-price = np.array([[2.6, 2.6], [1.3, 1.5], [4, 5], []])
+def comparador_min(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    minimo = np.min(lista_prices)
+    if lista_prices[0] == minimo:
+        return "wong", minimo
+    if lista_prices[1] == minimo:
+        return "metro", minimo
+    if lista_prices[2] == minimo:
+        return "plaza_vea", minimo
+    if lista_prices[3] == minimo:
+        return "tottus", minimo
+    if lista_prices[4] == minimo:
+        return "vivanda", minimo
+
+def comparador_max(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    maximo = np.max(lista_prices)
+    if lista_prices[0] == maximo:
+        return "wong", maximo
+    if lista_prices[1] == maximo:
+        return "metro", maximo
+    if lista_prices[2] == maximo:
+        return "plaza_vea", maximo
+    if lista_prices[3] == maximo:
+        return "tottus", maximo
+    if lista_prices[4] == maximo:
+        return "vivanda", maximo
+
+def variacion(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    lista_variaciones = []
+    for i in lista_prices:
+        if len(i) == 0:
+            variacion = None
+            lista_variaciones.append(variacion)
+        else:
+            fin = i[-1]
+            inicio = i[0]
+            variacion = ((fin-inicio)/inicio)*100
+            variacion = np.round(np.mean(variacion),1)
+            lista_variaciones.append(variacion)
+
+    return lista_variaciones
+
+price = np.array([[2.6, 2.6], [1.3, 1.5], [4, 5], ['nan','nan'], [9,8]])
 
 mins=[]
 maxs=[]
@@ -10,8 +54,92 @@ for i in price:
         mins.append(np.min(i))
         maxs.append(np.max(i))
     else:
-        mins.append(None)
-        maxs.append(None)
+        mins.append(99999)
+        maxs.append(0)
 
-print(maxs)
+print(variacion(price))
+'''
+'''
+import pandas as pd
+import numpy as np
+retail_data = pd.read_csv("retail_data_final.csv")
 
+description = retail_data['description'].to_numpy()
+inca = []
+for i in description:
+    if ("inca kola" in i) and "500ml" in i:
+        inca.append(i)
+
+print((inca))'''
+
+from flask import Flask, render_template
+import plotly.graph_objects as go
+import json
+import plotly
+import plotly.io as pio
+import chart_studio.plotly as py
+import numpy as np
+import datetime
+from pytz import timezone
+
+def create_fig(retailers, skus, title, price_evolution_data, time):   
+    ## create traces
+    fig = go.Figure()
+
+    #NUEVO
+    est = timezone('EST')
+    now = datetime.datetime.now(est) - datetime.timedelta(days=int(time))
+    year = '{:02d}'.format(now.year)
+    month = '{:02d}'.format(now.month)
+    day = '{:02d}'.format(now.day)
+    current_date = '{}-{}-{}'.format(year, month, day)
+    #####
+
+    #Save prices
+    prices = []
+    ## navigate for each retail and sku
+    for retail, sku in zip(retailers, skus):
+        ## select the sku and retail
+        query = price_evolution_data.loc[(price_evolution_data['sku'] == sku) & (price_evolution_data['retail'] == retail)]
+        query = query.loc[query["date"]>=current_date]
+
+        price = query["price"] ## get the price
+        date = query["date"] ## get the date
+        
+        fig.add_trace(go.Scatter(x=date.values,
+                                 y=price.values,                                
+                                 name=retail))
+
+        prices.append(price.values)
+        #[prices.append(i) for i in price.values]
+
+    fig.update_traces(mode='lines+markers')
+    fig.update_layout(title={
+                            'text': title,
+                            'y':0.9,
+                            'x':0.5,
+                            'xanchor': 'center',
+                            'yanchor': 'top'})
+    
+    return fig, prices
+
+retailers = ["wong", "metro", "plaza_vea", "tottus", "vivanda"]
+skus = ["59539001", "59539001", "497497", "10174358", "497497"]
+title = 'INKA COLA 500ML'
+import pandas as pd
+price_evolution_data = pd.read_csv("price_evolution.csv")
+time = "7"
+fig, price = create_fig(retailers, skus, title, price_evolution_data, time)
+fig.show(renderer="iframe")
+
+'''import datetime
+from pytz import timezone
+est = timezone('EST')
+now = datetime.datetime.now(est) - datetime.timedelta(days=7)
+year = '{:02d}'.format(now.year)
+month = '{:02d}'.format(now.month)
+day = '{:02d}'.format(now.day)
+current_date = '{}-{}-{}'.format(year, month, day)
+print(current_date)
+seven_days = price_evolution_data.loc[price_evolution_data["date"]>=current_date]
+print(seven_days)'''
--- index.py
+++ index.py
@@ -77,49 +77,107 @@ def pagina_principal():
 @app.route("/prueba")
 def pag_prueba():
     retailers = ["wong", "metro", "plaza_vea", "tottus", "vivanda"]
-    skus = ["59539001", "59539001", "497497", "inca-kola-gaseosa-10174358/p/", "497497"]
+    skus = ["59539001", "59539001", "497497", "10174358", "497497"]
     title = 'INKA COLA 500ML'
     fig, price = create_fig(retailers, skus, title, price_evolution_data)
-    fig.write_html("static/img/fig1.html")
+    fig.write_html("static/img/fig2.html") #NO HACE OVERWRITE, BUSCAR FORMA PARA HACERLO
 
-    mi,ma, mean, var_t, var_d = estadisticos(price)
+    #mi,ma, mean, var_t, var_d = estadisticos(price)
+    #return render_template('prueba.html', mi=mi, ma=ma, mean=mean,var_t=var_t,var_d=var_d)
 
-    return render_template('prueba.html', mi=mi, ma=ma, mean=mean,var_t=var_t,var_d=var_d)
+    mins, maxs, variacion = estadisticos(price)
+    retailers = ["WONG", "METRO", "PLAZA VEA", "TOTTUS", "VIVANDA"]
+
+
+    return render_template('prueba.html',mins = mins, maxs = maxs, variacion=variacion,retailers=retailers)
 
 '''@app.context_processor
 def add_imports():
-    return dict(numpy=numpy)
+    return dict(numpy=numpy)    
 '''
+
 def estadisticos(price):
-    print(price)
     maxs = []
     mins = []
     #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
     for i in price:
         if len(i)!=0:
-            mins.append(np.min(i))
-            maxs.append(np.max(i))
+            mins.append(np.nanmin(i))
+            maxs.append(np.nanmax(i))
         else:
-            mins.append(None)
-            maxs.append(None)
+            mins.append(99999)
+            maxs.append(0)
 
-    #Para estos 3, está bien juntar todos
-    #MINIMO, MAXIMO, PROMEDIO DE TODA LA SEMANA O EL PERIODO DE TIEMPO
-    minimo = np.min(price)
-    maximo = np.max(price)
-    promedio = np.round(np.mean(price),1)
+    mins = comparador_min(mins)
+    maxs = comparador_max(maxs)
+    var = variacion(price)
+    return mins, maxs, var
 
-    #Variación inicio a fin (NO ES DE TODOS JUNTOS)
-    inicio = price[0]
-    fin = price[-1]
-    variacion_total = ((fin-inicio)/inicio)*100
+'''#Para estos 3, está bien juntar todos
+#MINIMO, MAXIMO, PROMEDIO DE TODA LA SEMANA O EL PERIODO DE TIEMPO
+minimo = np.min(price)
+maximo = np.max(price)
+promedio = np.round(np.mean(price),1)
 
-    #Variacion ultimas 24h (NO ES DE TODOS JUNTOS)
-    inicio = price[-4]
-    fin = price[-1]
-    variacion_dia = ((fin-inicio)/inicio)*100
+#Variación inicio a fin (NO ES DE TODOS JUNTOS)
+inicio = price[0]
+fin = price[-1]
+variacion_total = ((fin-inicio)/inicio)*100
 
-    return minimo, maximo, promedio, variacion_total, variacion_dia
+#Variacion ultimas 24h (NO ES DE TODOS JUNTOS)
+inicio = price[-4]
+fin = price[-1]
+variacion_dia = ((fin-inicio)/inicio)*100
+
+return minimo, maximo, promedio, variacion_total, variacion_dia
+'''
+
+def comparador_min(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    minimo = np.nanmin(lista_prices)
+    if lista_prices[0] == minimo:
+        return "wong", minimo
+    if lista_prices[1] == minimo:
+        return "metro", minimo
+    if lista_prices[2] == minimo:
+        return "plaza_vea", minimo
+    if lista_prices[3] == minimo:
+        return "tottus", minimo
+    if lista_prices[4] == minimo:
+        return "vivanda", minimo
+
+def comparador_max(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    maximo = np.nanmax(lista_prices)
+    if lista_prices[0] == maximo:
+        return "wong", maximo
+    if lista_prices[1] == maximo:
+        return "metro", maximo
+    if lista_prices[2] == maximo:
+        return "plaza_vea", maximo
+    if lista_prices[3] == maximo:
+        return "tottus", maximo
+    if lista_prices[4] == maximo:
+        return "vivanda", maximo
+
+def variacion(lista_prices):
+    #1: WONG, 2: METRO, 3: PLAZA VEA, 4: TOTTUS Y 5: VIVANDA
+    lista_variaciones = []
+    for i in lista_prices:
+        if len(i) == 0:
+            variacion = None
+            lista_variaciones.append(variacion)
+        else:
+            fin = i[-1]
+            inicio = i[0]
+            variacion = ((fin-inicio)/inicio)*100
+            variacion = np.round(np.mean(variacion),1)
+            if np.isnan(variacion):
+                lista_variaciones.append(None)
+            else:
+                lista_variaciones.append(variacion)
+
+    return lista_variaciones
 
 if __name__ == '__main__':
     import pandas as pd
